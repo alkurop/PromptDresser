953ec876e5fc897c07a75ab0d5d7b74a98db575f
Alex <alkurop@gmail.com>
2025-08-08 13:50:41 +0200

    Fix mutual attenuation model imports

--- promptdresser/models/mutual_self_attention.py
+++ promptdresser/models/mutual_self_attention.py
@@ -18,7 +18,7 @@ from einops import rearrange
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 from diffusers.models.attention import BasicTransformerBlock
-from diffusers.models.unet_2d_blocks import CrossAttnDownBlock2D, CrossAttnUpBlock2D, DownBlock2D, UpBlock2D
+from diffusers.models.unets.unet_2d_blocks import CrossAttnDownBlock2D, CrossAttnUpBlock2D, DownBlock2D, UpBlock2D
 
 def torch_dfs(model: torch.nn.Module):
     result = [model]
